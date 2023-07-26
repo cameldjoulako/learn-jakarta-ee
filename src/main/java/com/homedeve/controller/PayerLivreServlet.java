6dbbde7108f3f0fadf857ef5583946d6e1264702
Camel Djoulako <cameldjoulako17@gmail.com>
2023-07-26 01:39:55 +0100

    Session intro setup page and class

--- src/main/java/com/homedeve/controller/PayerLivreServlet.java
+++ src/main/java/com/homedeve/controller/PayerLivreServlet.java
@@ -8,26 +8,25 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/livre")
-public class LivreServlet extends HttpServlet {
+@WebServlet("/payerLivre")
+public class PayerLivreServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         
-        String identifiantLivre = request.getParameter("id");
+        String numeroCarte = request.getParameter("numeroCarte");
         
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCtml>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet AboutServlet</title>");            
+            out.println("<title>Paiement éffectué</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h2>Merci d'avoir choisi le livre "+identifiantLivre+ "</h2> <br />");
-            out.print("<a href=\"payer-livre.html\">Clicker ici pour payer</a>");
+            out.println("<h2>Paiement effectué avec le numéro de carte: "+numeroCarte+ "</h2> <br />");
             out.println("</body>");
             out.println("</html>");
         }
