faa6918fe598062d82768ebe6fb40b0d9f663fbe
Camel Djoulako <cameldjoulako17@gmail.com>
2023-07-26 03:10:20 +0100

    Manipulation des session en jarkata ee

--- src/main/java/com/homedeve/controller/LivreServlet.java
+++ src/main/java/com/homedeve/controller/LivreServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -16,7 +17,14 @@ public class LivreServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         
+        HttpSession session = request.getSession();
+        
+        String numeroSession =  session.getId();
+        
+        
         String identifiantLivre = request.getParameter("id");
+        
+        session.setAttribute("identifiantLivre", identifiantLivre);
         
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -27,6 +35,7 @@ public class LivreServlet extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h2>Merci d'avoir choisi le livre "+identifiantLivre+ "</h2> <br />");
+            out.println("<h5>Votre numéro de session est: "+numeroSession+ "</h5> <br />");
             out.print("<a href=\"payer-livre.html\">Clicker ici pour payer</a>");
             out.println("</body>");
             out.println("</html>");
--- src/main/java/com/homedeve/controller/PayerLivreServlet.java
+++ src/main/java/com/homedeve/controller/PayerLivreServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -16,7 +17,12 @@ public class PayerLivreServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         
+        HttpSession session = request.getSession();
+        
+        String numeroSession =  session.getId();
         String numeroCarte = request.getParameter("numeroCarte");
+        
+        String identifiantLivre = (String) session.getAttribute("identifiantLivre");
         
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -27,6 +33,7 @@ public class PayerLivreServlet extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h2>Paiement effectué avec le numéro de carte: "+numeroCarte+ "</h2> <br />");
+            out.println("<h2>Le numéro du livre payé est : "+identifiantLivre+ "</h2> <br />");
             out.println("</body>");
             out.println("</html>");
         }
